4a17a529e3e860320c5f17d541b44555bf2e3520
Eduardo <eduardofares123@gmail.com>
2017-10-22 08:05:37 -0400

    Implemented passwords

--- lib/microblog_web/controllers/session_controller.ex
+++ lib/microblog_web/controllers/session_controller.ex
@@ -3,26 +3,39 @@ defmodule MicroblogWeb.SessionController do
 
     alias Microblog.Account
 
-    def login(conn, %{"email" => email}) do
-        user = Account.get_user_by_email!(email)
 
-        if user do
-            conn
-            |> put_session(:user_id, user.id)
-            |> put_flash(:info, "Logged in as #{user.email}")
-            |> redirect(to: message_path(conn, :index))
-        else
-            conn
-            |> put_session(:user_id, nil)
-            |> put_flash(:error, "No such user")
-            |> redirect(to: message_path(conn, :index))
-        end
+  def get_and_auth_user(email, password) do
+    user = Account.get_user_by_email!(email)
+    case Comeonin.Argon2.check_pass(user, password) do
+      {:ok, user} -> user
+      _else       -> nil
     end
+  end
 
-    def logout(conn, _params) do
-        conn
-        |> put_session(:user_id, nil)
-        |> put_flash(:info, "Logged out")
-        |> redirect(to: message_path(conn, :index))
+
+
+
+  def login(conn, %{"email" => email, "password" => password}) do
+    user = get_and_auth_user(email, password)
+
+
+    if user do
+      conn
+      |> put_session(:user_id, user.id)
+      |> put_flash(:info, "Logged in as #{user.email}")
+      |> redirect(to: message_path(conn, :index))
+    else
+      conn
+      |> put_session(:user_id, nil)
+      |> put_flash(:error, "Bad email/password")
+      |> redirect(to: message_path(conn, :index))
     end
+  end
+
+  def logout(conn, _params) do
+    conn
+    |> put_session(:user_id, nil)
+    |> put_flash(:info, "Logged out")
+    |> redirect(to: message_path(conn, :index))
+  end
 end
